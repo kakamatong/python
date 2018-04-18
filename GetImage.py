39c30de8c16b31bcff24ecb303574c21a7575fd5
kakamatong <631603920@qq.com>
2018-04-18 23:55:24 +0800

    优化代码

--- GetImage.py
+++ GetImage.py
@@ -2,20 +2,25 @@ import requests
 import re
 import io
 import sys
+import os
 import time
 
 def getBYImage():
     nowTime = time.strftime("%Y.%m.%d")
-    url = 'http://cn.bing.com/'
-    con = requests.get(url)
-    content = con.text
-
-    reg = r"(az/hprichbg/rb/.*?.jpg)"
-    a = re.findall(reg, content, re.S)[0]
-    imgUrl = url + a
-    print(imgUrl)
-    read = requests.get(imgUrl)
-    f = open('image/%s.jpg' % nowTime, 'wb')
-    f.write(read.content)
-    f.close()
-    pass
+    filePath = 'image/' + '%s.jpg' % nowTime
+    if os.path.exists(filePath):
+        print("isExists %s" % filePath)
+        pass
+    else:
+        url = 'http://cn.bing.com/'
+        con = requests.get(url)
+        content = con.text
+        reg = r"(az/hprichbg/rb/.*?.jpg)"
+        a = re.findall(reg, content, re.S)[0]
+        imgUrl = url + a
+        print(imgUrl)
+        read = requests.get(imgUrl)
+        f = open(filePath, 'wb')
+        f.write(read.content)
+        f.close()
+        pass
