675d6914a11744189f9ab5ade08e76ca0bbf8e7d
kakamatong <631603920@qq.com>
2018-04-18 23:45:57 +0800

    获取必应图片代码修改

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 
 from ToolUtils import ToolUtils
+import GetImage
 def main():
     print("main")
     def printInfo():
-        print("this is test for timer forever")
+        #print("this is test for timer forever")
+        GetImage.getBYImage()
     ToolUtils.timerForever(1, printInfo)
 
-    def printInfo2():
-        print("this is test for timer2")
-    ToolUtils.timer(5, printInfo2)
+    # def printInfo2():
+    #     print("this is test for timer2")
+    # ToolUtils.timer(5, printInfo2)
     pass
 
 if __name__ == "__main__":
