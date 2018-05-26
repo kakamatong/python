3f7d764650b8d309814ed28f8dd4e6962ae11b12
wlj_tenxunyun <631603920@qq.com>
2018-05-26 17:05:08 +0800

    update weChat.py

--- WeChat.py
+++ WeChat.py
@@ -1,2 +1,10 @@
 import os
+import itchat
 
+myTxt = "嗨！我是卡卡马桶的微信小号秋刀鱼君^_^，功能还未完善，有事请联系15067128015"
+@itchat.msg_register(itchat.content.TEXT)
+def text_reply(msg):
+	return myTxt
+
+itchat.auto_login(enableCmdQR=2)
+itchat.run()
