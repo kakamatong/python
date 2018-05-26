72136ac2ba9371f943bceaaab22bbec632ca39e5
wlj_tenxunyun <631603920@qq.com>
2018-05-26 17:26:03 +0800

    updata wechat.py

--- WeChat.py
+++ WeChat.py
@@ -1,10 +1,13 @@
+#coding:utf-8
 import os
 import itchat
 
+myTxt2 = "adfsdfasdfas"
 myTxt = "嗨！我是卡卡马桶的微信小号秋刀鱼君^_^，功能还未完善，有事请联系15067128015"
+strTxt = unicode(myTxt,"utf-8")
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
-	return myTxt
+	return strTxt
 
 itchat.auto_login(enableCmdQR=2)
 itchat.run()
